7ba4e93b7e6b94f348666a67e094b4db826178a1
mayankmonani7 <63096017+mayankmonani7@users.noreply.github.com>
2020-12-01 16:31:45 +0530

    initial commit

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <hi className="section-title">about us</hi>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
+        asperiores illo veniam pariatur animi dignissimos exercitationem optio
+        odio in explicabo qui ea expedita architecto voluptate iusto neque
+        dolor, quis odit molestiae itaque ipsum? Exercitationem enim qui
+        doloribus beatae id doloremque.
+      </p>
+    </section>
+  );
+};
+
+export default About;
